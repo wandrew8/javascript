6bd4f8f28be178c64ba0c6e8194d6c7e331d4b41
Andrew Weiss <wandrew8@gmail.com>
2021-09-01 22:18:34 -0700

    begins adding template web components

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
-//Global Variables
-const mainButton = document.querySelector('#click');
+// //Global Variables
+// const template = document.querySelector('#first-template');
+// const container = document.querySelector("#container");
+// const clone = template.content.firstElementChild.cloneNode(true);
+// const clone2 = template.content.cloneNode(true);
 
-//Functions
-function doCoolThing() {
-    const title = document.querySelector('h1');
-    title.textContent = "Hello!"
+// //Event listeners
+// clone.addEventListener("click", function () {
+//     console.log("You clicked the first clone");
+// })
+
+// clone2.addEventListener("click", function () {
+//     console.log("You clicked the second clone");
+// })
+
+
+// //Appends the template element to the DOM
+// container.appendChild(clone);
+// container.appendChild(clone2);
+
+class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        const heading = document.createElement('h2');
+        heading.textContent = "Hello World!";
+        shadow.appendChild(heading);
+    }
 }
 
-// Event Listeners
-mainButton.addEventListener('click', doCoolThing)
+customElements.define("hello-world", HelloWorld);
+
+
